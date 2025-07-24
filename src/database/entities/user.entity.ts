53bc202afa0cd88768ba291de2bad027a5a2c313
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-07-25 02:08:31 +0300

    migrate: renamed column in users table

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -7,7 +7,7 @@ import {
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
-  email: varchar('email', { length: 256 }).unique().notNull(),
+  username: varchar('username', { length: 256 }).unique().notNull(),
   firstName: varchar('first_name', { length: 256 }).notNull(),
   lastName: varchar('last_name', { length: 256 }),
   password: text('password').notNull(),
