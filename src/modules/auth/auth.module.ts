fa7f57414448f57bb246947a0b781af818f4d705
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-07-25 17:18:37 +0300

    bugfix: added jwt guard to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtService } from '@nestjs/jwt';
+import { AccessStrategy } from './strategies/access.strategy';
 
 @Module({
   imports: [PassportModule],
@@ -12,6 +13,7 @@ import { JwtService } from '@nestjs/jwt';
     AuthService,
     JwtService,
     LocalStrategy,
+    AccessStrategy,
   ],
 })
 export class AuthModule {}
